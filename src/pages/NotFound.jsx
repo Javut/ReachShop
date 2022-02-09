efb507df31fb2f5b312385c925e5affc672e0519
javut <jaider.vergara@globalbank.com.pa>
2022-02-08 20:54:00 -0500

    first commit

---
+++ src/pages/NotFound.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const NotFounf = () => {
+    return ( 
+            <div>
+
+           <h2>No encontrado, error 404</h2> 
+
+            </div>
+
+
+     );
+}
+ 
+export default NotFounf;
